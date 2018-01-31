5f8b5218f01c058a448d8f38afb04b4e6540ac3b
ExtremeSoul <clegutko@gmail.com>
2018-01-31 22:27:17 +0100

    1st test added

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,2 +1,18 @@
 public class WordCounter {
+
+
+    private final String sentence;
+
+    public WordCounter(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public int count(String word) {
+        int counter = 0;
+        if (word.equals(sentence)) {
+
+            counter++;
+        }
+        return counter;
+    }
 }
--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WordCounterTest {
+    private static final int ZERO = 0;
+
+    @Test
+    public void shouldReturnZeroWhenOneWordSentenceWithoutHelloGiven() {
+        WordCounter wordCounter = new WordCounter("Great World");
+        long result = wordCounter.count("Hello");
+        assertEquals(ZERO, result);
+    }
+
 }
